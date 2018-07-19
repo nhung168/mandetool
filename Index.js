2cea4e2512845c4851b6407dd1c4364d220c8f35
nhung168 <nhung168@users.noreply.github.com>
2018-07-20 00:35:10 +0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,6 +1,15 @@
-function querySheets() {
-  var sheets = workbook.getPublishedSheetsInfo();
-  var text = getSheetsAlertText(sheets);
-  text = "Sheets in the workbook:\n" + text;
-  alert(text);
-}    
+function initializeViz() {
+  var placeholderDiv = document.getElementById("tableauViz");
+  var url = "https://public.tableau.com/views/Demo-THX/Dashboard1?:embed=y&:display_count=yes&publish=yes";
+  var options = {
+    width: placeholderDiv.offsetWidth,
+    height: placeholderDiv.offsetHeight,
+    hideTabs: true,
+    hideToolbar: true,
+    onFirstInteractive: function () {
+      workbook = viz.getWorkbook();
+      activeSheet = workbook.getActiveSheet();
+    }
+  };
+  viz = new tableau.Viz(placeholderDiv, url, options);
+}   
