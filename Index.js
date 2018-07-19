c3f58725452e852570783afeaf3a541e43e869ce
Nhung <nhung168@gmail.com>
2018-07-20 00:44:36 +0700

    Version 2

--- Index.js
+++
@@ -1,6 +0,0 @@
-function querySheets() {
-  var sheets = workbook.getPublishedSheetsInfo();
-  var text = getSheetsAlertText(sheets);
-  text = "Sheets in the workbook:\n" + text;
-  alert(text);
-}    
