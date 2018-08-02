5c6141cdd820804ac1138f32368a4f4b5b3351ae
nhung168 <nhung168@users.noreply.github.com>
2018-08-02 21:43:02 +0700

    Delete Index.js

--- Index.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-  var vizDiv = document.getElementByID('viz');
-  var vizURL = "https://public.tableau.com/shared/JJ2JNPJWP?:display_count=yes";
-  var options = {
-       width : '600px',
-       height: '540px',
-        };
-   viz = new tableaauSoftware.Viz(vizDiv, vizURL, options);
-}   
